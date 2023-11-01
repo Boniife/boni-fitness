237526560e04926bc3280464f2fb335e8947ca14
boniife <131681425+Boniife@users.noreply.github.com>
2023-11-01 02:21:26 +0100

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
         <img src={Logo} alt="logo" width="100px" height="40px" />
         <Typography variant="h5" pb="40px" mt="20px">
-          Made By Ifeanyi
+          Boni-Fitness
         </Typography>
       </Stack>
     </Box>
